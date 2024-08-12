a1537b2f945a506a78bc45d8d6dba0c9a6602f53
Anit <anitbani8096@gmail.com>
2024-08-12 11:08:26 +0530

    While loop

--- 01_Basic/Java12.java
+++ 01_Basic/Java12.java
@@ -2,6 +2,9 @@ import java.util.Scanner;;
 
 public class Java12 {
     public static void main(String[] args) {
+        /*
+         * While loop executes a block of code until it's condition remains true
+         */
         Scanner inpt = new Scanner(System.in);
         String Name = "";
 
