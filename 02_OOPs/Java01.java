69bd79bc4fff80939efa1e56dcdafc7a956cf440
Anit <anitbani8096@gmail.com>
2024-08-26 10:50:59 +0530

    Constructors

---
+++ 02_OOPs/Java01.java
@@ -0,0 +1,26 @@
+public class Java01 {
+    public static void main(String[] args) {
+        /*
+         * A constructor is a special method that is called when an objet of a 
+         * class is initiated/created
+         */
+        Car c1 = new Car("Mahindra", "XUV500", 2020, "Silver", 1200000D);
+        System.out.println(c1.Model);
+    }
+}
+
+class Car {
+    String Make;
+    String Model;
+    int Year;
+    String color; 
+    Double Price;
+
+    Car(String MAKE, String MODEL, int YEAR, String COLOR, Double PRICE) {
+        Make = MAKE;
+        Model = MODEL;
+        Year = YEAR;
+        color = COLOR;
+        Price = PRICE;
+    }
+}
