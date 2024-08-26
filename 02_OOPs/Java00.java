ad2f59fd909a1a5b4c37280cb483402b004ca8ae
Anit <anitbani8096@gmail.com>
2024-08-26 10:49:17 +0530

    Objects

--- 02_OOPs/Java00.java
+++ 02_OOPs/Java00.java
@@ -1,6 +1,6 @@
 public class Java00 {
     public static void main(String[] args) {
-        //An Object in aa instamce of a class that may contain attributes & methods 
+        //An Object in aa instance of a class that may contain attributes & methods 
         //example: (phone, desk, computer, coffee mug)
         Cars c1 = new Cars();
         System.out.println(c1.Make);
