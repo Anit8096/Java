06cf783e994f3e5e87e47e7f6b69e48d1f050a88
Anit <anitbani8096@gmail.com>
2024-08-26 13:19:05 +0530

    Objects

--- 02_OOPs/Cars.java
+++ 02_OOPs/Cars.java
@@ -5,6 +5,13 @@ public class Cars {
     String color = "Blue"; 
     Double Price = 346834.42;
 
+    public String toString() {  // this will be used in file: Java31
+        // String myString = Make +"\n" + Model + "\n" + color + "\n" + Year + "\n"; 
+        // return myString;    
+        //                   OR do it as shown below for simpler approach
+        return Make +"\n" + Model + "\n" + color + "\n" + Year + "\n";
+    }
+
     void Start() {
         System.out.println("Engin started");
     }
