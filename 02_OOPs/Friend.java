f6754352edffd093d13af76ff2b53ecd08738957
Anit <anitbani8096@gmail.com>
2024-08-29 00:43:23 +0530

    Static Modifier

---
+++ 02_OOPs/Friend.java
@@ -0,0 +1,10 @@
+public class Friend {
+    // Used in file Named Java33.java
+    String name;
+    static int Number_of_friends;
+
+    Friend(String NME) {
+        this.name = NME;
+        Number_of_friends++;
+    }
+}
---
+++ 02_OOPs/Java33.java
@@ -0,0 +1,17 @@
+public class Java33 {
+    public static void main(String[] args) {
+        // ststic = keyword modifier. a single copy of variable/method is created and is shared b/w all instances 
+        // of the class. The class "owns" that static method/variable
+
+        /* Comment out the following 3 lines */
+        // Friend F1 = new Friend("Jay");
+        // Friend F2 = new Friend("Anav");
+        // Friend F3 = new Friend("Arpit Bansal");
+        System.out.println("Number of friends I have: " + Friend.Number_of_friends);
+    }
+}
+
+/* 
+ * Note: static member of a class can be accessed by an object of that class though 
+ * it is not recommended to do so.
+ */
